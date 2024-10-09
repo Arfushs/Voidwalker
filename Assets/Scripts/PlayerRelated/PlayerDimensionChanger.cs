74d073e5758b014770adee78730080233e3c0ea5
Arfushs <36006828+Arfushs@users.noreply.github.com>
2024-10-09 23:35:01 +0300

    update

--- Assets/Scripts/PlayerRelated/PlayerDimensionChanger.cs
+++ Assets/Scripts/PlayerRelated/PlayerDimensionChanger.cs
@@ -49,7 +49,7 @@ public class PlayerDimensionChanger : MonoBehaviour
     {
         Vector3 finalScale = new Vector3(50, 50, 50);
         Sequence sequence = DOTween.Sequence();
-        sequence.Append(_spriteMask.DOScale(finalScale, _dimensionChangeDuration).SetEase(Ease.OutBounce))
+        sequence.Append(_spriteMask.DOScale(finalScale, _dimensionChangeDuration).SetEase(Ease.OutQuad))
             .OnComplete(() => CloseSpriteMask());
     }
 
