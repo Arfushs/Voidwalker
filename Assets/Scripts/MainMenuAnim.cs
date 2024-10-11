96e6d8cf28812cb36409d30f5003da8caf379ca2
Arfushs <36006828+Arfushs@users.noreply.github.com>
2024-10-12 01:21:39 +0300

    Main Menu Cutscene

--- Assets/Scripts/MainMenuAnim.cs
+++ Assets/Scripts/MainMenuAnim.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +10,7 @@ public class MainMenuAnim : MonoBehaviour
 {
     [SerializeField] private Image _uiBackground;
     [SerializeField] private Image _title;
+    [SerializeField] private TextMeshProUGUI _startText, _exitText, _optionsText;
     [SerializeField] private float _fadeDuration = 0.5f;
 
 
@@ -23,6 +25,9 @@ public class MainMenuAnim : MonoBehaviour
         {
             _title.DOFade(1f, _fadeDuration);
             _uiBackground.DOFade(.89f, _fadeDuration);
+            _startText.DOFade(1f, _fadeDuration);
+            _exitText.DOFade(1f, _fadeDuration);
+            _optionsText.DOFade(1f, _fadeDuration);
         }
         
     }
